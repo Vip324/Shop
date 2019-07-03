ae1ca2c010db4f47c9656e2ff38c4854310d8d16
Vip324 <givpost@gmail.com>
2019-07-03 15:36:27 +0300

    изменил методы delete и save в корзине

--- basketapp/models.py
+++ basketapp/models.py
@@ -2,7 +2,21 @@ from django.db import models
 from django.conf import settings
 from mainapp.models import Product
 
+
+# Менеджер модели
+class BasketQuerySet(models.QuerySet):
+    # удаление набора данных
+    def delete(self, *args, **kwargs):
+        for object in self:
+            object.product.quantity += object.quantity
+            object.product.save()
+        super().delete(*args, **kwargs)
+
 class Basket(models.Model):
+
+    # 1. Переопределение базовго менеджера
+    objects = BasketQuerySet.as_manager()
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField(verbose_name='количество', default= 0 )
@@ -32,4 +46,37 @@ class Basket(models.Model):
         [basket_items_dic.update({item.product: item.quantity}) for item in basket_items]
 
         return basket_items_dic
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.filter(pk=pk).first()
+
+    # переопределяем метод, удаляющий объект
+    def delete(self):
+        print('удаление объекта корзины') #?
+        self.product.quantity += self.quantity
+        self.product.save()
+        super().delete()
+
+    def save(self, *args, **kwargs):
+        # 1. Создали новый объект корзины
+        # 2. Измененеие объекта
+
+        # как понять изменяем объект или создаем? - pk
+        if self.pk:
+            # изменяем объект
+            # 1. Сколько осталось в корзине
+            # 2. Сколько было в корзине
+            # print('сколько осталось в конризе:', self.quantity)
+
+            # Где взять старое значение
+            # print('сколько было (старое значение)', Basket.objects.get(pk=self.pk).quantity)
+            # print('сколько было (старое значение)', self.__class__.get_item(self.pk).quantity)
+
+            self.product.quantity -= self.quantity - self.__class__.get_item(self.pk).quantity
+        else:
+            # создаем объект
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super(self.__class__, self).save(*args, **kwargs)
     
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from django.db import models
+
+class AktivManager(models.QuerySet):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
 class ProductCategory (models.Model):
+    object = models.manager
+    active_objects = AktivManager
+
     name = models.CharField(verbose_name= 'Category name' , max_length= 64 , unique= True )
     description = models.TextField(verbose_name= 'Description' , blank= True )
     is_active = models.BooleanField(verbose_name='activ status', default=True)
@@ -10,6 +18,9 @@ class ProductCategory (models.Model):
         return self.name
 
 class Product (models.Model):
+    object = models.manager
+    active_objects = AktivManager
+
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name= 'Product name' , max_length= 128 )
     image = models.ImageField(upload_to= 'products_images' , blank= True )
@@ -20,4 +31,4 @@ class Product (models.Model):
     is_active = models.BooleanField(verbose_name='activ status', default=True)
     
     def __str__ (self):
-        return f" {self.name} ( {self.category.name} )"
+        return f" {self.name} ( {self.category.name}, {self.quantity} )"
